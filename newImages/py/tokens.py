a2d5840210fb9baffd05c491546906381e701b3f
Sadamune <zwiebelshi@gmail.com>
2020-12-10 00:11:42 +0900

    fix colors of tokens

--- newImages/py/tokens.py
+++ newImages/py/tokens.py
@@ -34,17 +34,17 @@ def create_token(country, type):
                        (front.height - back.height) // 2), back)
     token.paste(front, (0, 0), front)
 
-    token.save("../Tokens/" + type + "_" + country.name + ".png")
+    token.save("../Tokens/QMG_" + type + "_" + country.name + ".png")
 
 
 country_list = []
 country_list.append(TokenCountry('US', (25, 25, 85), (192, 192, 192)))
-country_list.append(TokenCountry('USSR', (0, 0, 0), (155, 0, 0)))
+country_list.append(TokenCountry('USSR', (0, 0, 0), (170, 0, 0)))
 country_list.append(TokenCountry('UK', (0, 0, 0), (220, 180, 0)))
 country_list.append(TokenCountry('CN', (50, 30, 140), (235, 54, 54)))
-country_list.append(TokenCountry('FR', (100, 0, 0), (120, 120, 255)))
-country_list.append(TokenCountry('GR', (0, 0, 0), (125, 125, 125)))
-country_list.append(TokenCountry('ITA', (0, 0, 0), (155, 0, 225)))
+country_list.append(TokenCountry('FR', (60, 60, 180), (125, 255, 125)))
+country_list.append(TokenCountry('GR', (0, 0, 0), (140, 140, 140)))
+country_list.append(TokenCountry('ITA', (0, 0, 0), (125, 65, 145)))
 country_list.append(TokenCountry('JP', (190, 0, 38), (240, 240, 240)))
 for country in country_list:
     for type in ['Airforce', 'Army', 'Navy']:
