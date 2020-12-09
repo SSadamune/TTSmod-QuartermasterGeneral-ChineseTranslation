90132f7c57fce61b29d8fbb079fdfb7ce6ade6af
Sadamune <zwiebelshi@gmail.com>
2020-12-09 23:06:34 +0900

    generate new tokens

--- newImages/py/tokens.py
+++ newImages/py/tokens.py
@@ -6,6 +6,8 @@ class TokenCountry:
         self.name = name
         self.front_color = front_color
         self.back_color = back_color
+        self.faction = 'Axis' if self.name in ['GR', 'ITA', 'JP'
+                                               ] else 'Alliens'
 
 
 def change_color(image, color):
@@ -21,14 +23,13 @@ def change_color(image, color):
 
 
 def create_token(country, type):
-
     back = Image.open("../resources/token/inner.png")
-    front = Image.open("../resources/token/" + type + ".png")
+    front = Image.open("../resources/token/" + type + "_" + country.faction +
+                       ".png")
     token = Image.new(front.mode, (front.width, front.height))
     front = change_color(front, country.front_color)
     back = change_color(back, country.back_color)
-    if country.name in ['GR', 'ITA', 'JP']:
-        front = front.transpose(Image.FLIP_LEFT_RIGHT)
+
     token.paste(back, ((front.width - back.width) // 2,
                        (front.height - back.height) // 2), back)
     token.paste(front, (0, 0), front)
@@ -37,13 +38,13 @@ def create_token(country, type):
 
 
 country_list = []
-country_list.append(TokenCountry('US', (15, 0, 125), (225, 225, 225)))
-country_list.append(TokenCountry('USSR', (0, 0, 0), (170, 0, 0)))
+country_list.append(TokenCountry('US', (25, 25, 85), (192, 192, 192)))
+country_list.append(TokenCountry('USSR', (0, 0, 0), (155, 0, 0)))
 country_list.append(TokenCountry('UK', (0, 0, 0), (220, 180, 0)))
 country_list.append(TokenCountry('CN', (50, 30, 140), (235, 54, 54)))
-country_list.append(TokenCountry('FR', (235, 235, 235), (65, 65, 235)))
-country_list.append(TokenCountry('GR', (150, 150, 150), (0, 0, 0)))
-country_list.append(TokenCountry('ITA', (15, 15, 15), (141, 15, 193)))
+country_list.append(TokenCountry('FR', (100, 0, 0), (120, 120, 255)))
+country_list.append(TokenCountry('GR', (0, 0, 0), (125, 125, 125)))
+country_list.append(TokenCountry('ITA', (0, 0, 0), (155, 0, 225)))
 country_list.append(TokenCountry('JP', (190, 0, 38), (240, 240, 240)))
 for country in country_list:
     for type in ['Airforce', 'Army', 'Navy']:
